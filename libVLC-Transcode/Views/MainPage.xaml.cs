ee126471150d61b210347b78afd7dc2b1f718391
Anthony Wilkey <groovykool@gmail.com>
2019-10-21 17:49:10 -0500

    Codec Experiments

--- libVLC-Transcode/Views/MainPage.xaml.cs
+++ libVLC-Transcode/Views/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel.Core;
 using Windows.Storage;
 using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace libVLC_Transcode.Views
 {
@@ -16,6 +17,7 @@ namespace libVLC_Transcode.Views
     MediaPlayer mprec;
     LibVLC rLIB;
     Media mrec;
+    int lines = 0;
     public MainPage()
     {
       InitializeComponent();
@@ -41,31 +43,36 @@ namespace libVLC_Transcode.Views
       StorageFolder tempFolder = ApplicationData.Current.TemporaryFolder;
 
       var pathstring = tempFolder.Path + "\\recordtemp.mp4";
-      
+
 
       //var urlstring = "http://194.103.218.16/mjpg/video.mjpg";
-      var urlstring = "rtsp://public:public@hyc.homeip.net/cam/realmonitor?channel=1&subtype=1";
-
+      //var urlstring = "rtsp://public:public@hyc.homeip.net/cam/realmonitor?channel=1&subtype=1";
+      var urlstring = "http://24.43.239.50/mjpg/video.mjpg";
+      //var urlstring = "rtsp://:@tonyw.selfip.com:6001/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif";
       rLIB = new LibVLC();
       mprec = new MediaPlayer(rLIB);
       mrec = new Media(rLIB, urlstring, FromType.FromLocation);
       //assign log event handler
       rLIB.Log += (sender, ee) => log(ee);
-     
-      var option = $":sout=#transcode{{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}}:std{{access=file,mux=mp4,dst={pathstring}}}";
-      //var option = $":sout=#file{{dst={pathstring}}}";
+      //sout=#transcode{vcodec=hevc,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:std{access=file{no-overwrite},mux=mp4,dst='C:/Users/groov/Videos/tester3.mp4'}
+      //transcode{vcodec=theo,vb=800,scale=Auto,acodec=vorb,ab=128,channels=2,samplerate=8000,scodec=none}:std{access=file{no-overwrite},mux=ogg,dst='C:/Users/groov/Videos/test3.ogv'}'
+      //var option = $":sout=#transcode{{vcodec=hevc,vb=800,scale=Auto,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}}:std{{access=file,mux=mp4,dst={pathstring}}}";
+      //var option = $":sout=#file{{mux=mp4,dst={pathstring}}}";
+      //var option = $":sout=#transcode{{vcodec=mp2v,vb=3000,scale=Auto,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}}:std{{access=file,mux=ts,dst={pathstring}}}";
+      var option = $":sout=#transcode{{vcodec=mp4v,vb=2000,scale=Auto,acodec=mp4a,ab=128,channels=2,samplerate=44100,scodec=none}}:std{{access=file,mux=mp4,dst={pathstring}}}";
       OT.Text += $"Option was added: {option} \n";
       mrec.AddOption(option);
+      //mrec.AddOption(":sout-keep");
 
     }
     private void RecordDipose()
     {
-      
+
       mprec.Stop();
       mprec.Dispose();
       mrec.Dispose();
       rLIB.Dispose();
-      
+
     }
 
     public async void log(LogEventArgs ee)
@@ -73,10 +80,16 @@ namespace libVLC_Transcode.Views
 
       //Debug.WriteLine($"[{ee.Level}] {ee.Module}:{ee.Message}");
       //Run on UIthread to write to control
+      lines++;
+      
       await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
       {
-
-        OT.Text += $"libVLC:[{ee.Level}] {ee.Module}:{ee.Message}" + "\n";
+        if (lines > 1000)
+        {
+          OT.Text = "";
+          lines = 0;
+        }
+        OT.Text += $"libVLC:[Line:{lines}][{ee.Level}] {ee.Module}:{ee.Message}\n";
 
       });
 
@@ -121,9 +134,9 @@ namespace libVLC_Transcode.Views
           OT.Text += "Record Success: File Saved in Pictures Library:libVLC-Transcode " + copiedFile.Name + "\n";
         }
       }
-     
-     
-     
+
+
+
 
     }
     public event PropertyChangedEventHandler PropertyChanged;
@@ -146,5 +159,20 @@ namespace libVLC_Transcode.Views
     }
 
     private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private void OT_TextChanged(object sender, TextChangedEventArgs e)
+    {
+     
+        //Keep log text box scrolled to the bottom
+        var grid = (Grid)VisualTreeHelper.GetChild(OT, 0);
+        for (var i = 0; i <= VisualTreeHelper.GetChildrenCount(grid) - 1; i++)
+        {
+          object obj = VisualTreeHelper.GetChild(grid, i);
+          if (!(obj is ScrollViewer)) continue;
+          ((ScrollViewer)obj).ChangeView(0.0f, ((ScrollViewer)obj).ExtentHeight, 1.0f);
+          break;
+        }
+     
+    }
   }
 }
